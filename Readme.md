c865b255527afde1b94fe87449509662012a491c
root <root@fichtenbackbone-3.freifunk-mk.de>
2016-02-28 19:24:50 +0100

    changed from templates to here-script

--- Readme.md
+++ Readme.md
@@ -1,9 +1,13 @@
 # Konfigurationshelfer für Freifunk BGP-Konzentrator Setup
 
-Das Script *create_configs.sh* fragt die nötigen Parameter ab und
-erzeugt daraus die Konfigurationsdateien.
+Das Script *bgp-konzentrator-setup.sh* fragt die nötigen Parameter ab und
+erzeugt daraus die Konfigurationsdateien:
+  * bird.conf
+  * bird6.conf
+  * Auszug aus interfaces
+  * ferm.conf
 
-Es ist möglich in der Datei *bgp-konzentrator.confi* die passenden
+Es ist möglich in der Datei *bgp-konzentrator.conf* die passenden
 Werte einzutragen. Damit ist dann später eine maschinelle
 Erstellung der Konfigs möglich.
 
---
+++ bgp-konzentrator-setup.sh
@@ -0,0 +1,308 @@
+#!/bin/bash
+
+CONFIGFILE=bgp-konzentrator.conf
+[ -r ${CONFIGFILE} ] && . ${CONFIGFILE}
+
+EXT=bgp
+
+function myread {
+	local prompt=$1
+	local default=$2
+	read -p "${prompt} (${default}): " value
+	echo $(sed 's/\//\\\\\//g' <<< ${value:-${default}})
+}
+
+
+function show_bird_config {
+	cat << _EOF > bird.conf.${EXT}
+router id ${MY_FFRL_EXIT_IPV4};
+protocol direct announce {
+        table master;
+        import where net ~ [${MY_FFRL_EXIT_IPV4}/32];
+        interface "tun-ffrl-uplink";
+};
+protocol kernel {
+        table master;
+        device routes;
+        import none;
+        export filter {
+                krt_prefsrc = ${MY_FFRL_EXIT_IPV4};
+                accept;
+        };
+        kernel table 42;
+};
+protocol device {
+        scan time 15;
+};
+function is_default() {
+        return (net ~ [0.0.0.0/0]);
+};
+template bgp uplink {
+        local as ${MY_AS_NUMBER};
+        import where is_default();
+        export where proto = "announce";
+};
+protocol bgp ffrl_ber_a from uplink {
+        source address ${BER_A_GRE_MY_IPV4}
+        neighbor ${BER_A_GRE_BB_IPV4} as ${ffrl_as_number};
+};
+protocol bgp ffrl_ber_b from uplink {
+        source address ${BER_B_GRE_MY_IPV4};
+        neighbor ${BER_B_GRE_BB_IPV4} as ${ffrl_as_number};
+};
+protocol bgp ffrl_dus_a from uplink {
+        source address ${DUS_A_GRE_MY_IPV4};
+        neighbor ${DUS_A_GRE_BB_IPV4} as ${ffrl_as_number};
+        preference 110;
+};
+protocol bgp ffrl_dus_b from uplink {
+        source address ${DUS_B_GRE_MY_IPV4};
+        neighbor ${DUS_B_GRE_BB_IPV4} as ${ffrl_as_number};
+        preference 110;
+};
+_EOF
+}
+
+function show_interfaces {
+	cat << _EOF >interfaces.${EXT}
+# Konfiguration Backbone-Anbindung fuer eigene FFRL Exit-IP
+auto eth1
+iface eth1 inet static
+	address 172.31.254.254
+	netmask 255.255.255.0
+
+auto tun-ffrl-uplink
+iface tun-ffrl-uplink inet static
+	address ${my_ffrl_exit_ipv4}
+	netmask 255.255.255.255
+	pre-up ip link add \$IFACE type dummy
+	post-down ip link del \$IFACE
+
+# Konfiguration Backbone-Anbindung Berlin A
+auto  tun-ffrl-ber-a 
+iface		tun-ffrl-ber-a inet tunnel
+	mode		gre
+	netmask		255.255.255.254
+	address		${BER_A_GRE_MY_IPV4}
+	dstaddr		${BER_A_GRE_BB_IPV4}
+	endpoint	185.66.195.0 
+	local		${MY_PUBLIC_IPV4}
+	ttl		255
+	mtu		1400
+	post-up ip -6 addr add ${BER_A_GRE_MY_IPV6} dev \$IFACE
+
+# Konfiguration Backbone-Anbindung Berlin B
+auto  tun-ffrl-ber-b 
+iface	tun-ffrl-ber-b inet tunnel
+	mode		gre
+	netmask		255.255.255.254
+	address		${BER_B_GRE_MY_IPV4}
+	dstaddr		${BER_B_GRE_BB_IPV4}
+	endpoint	185.66.195.1
+	local		${MY_PUBLIC_IPV4}
+	ttl		255
+	mtu		1400
+	post-up ip -6 addr add ${BER_B_GRE_MY_IPV6} dev \$IFACE
+
+# Konfiguration Backbone-Anbindung Duesseldorf A
+auto  tun-ffrl-dus-a 
+iface	tun-ffrl-dus-a inet tunnel
+	mode		gre
+	netmask		255.255.255.254
+	address		${DUS_A_GRE_MY_IPV4}
+	dstaddr		${DUS_A_GRE_BB_IPV4}
+	endpoint	185.66.193.0
+	local		${MY_PUBLIC_IPV4}
+	ttl		255
+	mtu		1400
+	post-up ip -6 addr add ${DUS_A_GRE_MY_IPV6} dev \$IFACE
+
+# Konfiguration Backbone-Anbindung Duesseldorf B
+auto  tun-ffrl-dus-b
+iface	tun-ffrl-dus-b inet tunnel
+	mode		gre
+	netmask		255.255.255.254
+	address		${DUS_B_GRE_MY_IPV4}
+	dstaddr		${DUS_B_GRE_BB_IPV4}
+	endpoint	185.66.193.1
+	local		${MY_PUBLIC_IPV4}
+	ttl		255
+	mtu		1400
+	post-up ip -6 addr add ${DUS_B_GRE_MY_IPV6} dev \$IFACE
+_EOF
+}
+
+function show_bird6_config {
+	cat << _EOF >bird6.conf.${EXT}
+log syslog { info };
+debug protocols { states, routes, filters, interfaces, events, packets };
+router id ${my_ffrl_exit_ipv4};
+protocol direct {
+        interface "-eth0", "*";  # Restrict network interfaces it works with
+}
+protocol kernel {
+        device routes;
+        import none;
+        export all;             # Default is export none
+        kernel table 42;        # Kernel table to synchronize with (default: main)
+}
+protocol device {
+        scan time 10;           # Scan interfaces every 10 seconds
+}
+function is_default() {
+        return (net ~ [::/0]);
+}
+filter hostroute {
+        if net ~ [2a03:2260:120::/56] then accept;        # Iserlohn
+        if net ~ [2a03:2260:120:100::/56] then accept;    # Altena
+        if net ~ [2a03:2260:120:200::/56] then accept;    # Meinerzhagen
+        if net ~ [2a03:2260:120:300::/56] then accept;    # Hemer
+        if net ~ [2a03:2260:120:400::/56] then accept;    # Luedenscheid
+        reject;
+}
+template bgp uplink {
+        local as ${MY_AS_NUMBER};
+        import where is_default();
+        export filter hostroute;
+        gateway recursive;
+}
+protocol bgp ffrl_ber_a from uplink {
+        description "Rheinland Backbone Berlin A";
+        source address ${BER_A_GRE_MY_IPV6};
+        neighbor ${BER_A_GRE_BB_IPV6} as ${ffrl_as_number};
+}
+protocol bgp ffrl_ber_b from uplink {
+        description "Rheinland Backbone Berlin B";
+        source address ${BER_B_GRE_MY_IPV6};
+        neighbor ${BER_B_GRE_BB_IPV6} as ${ffrl_as_number};
+}
+protocol bgp ffrl_dus_a from uplink {
+        description "Rheinland Backbone Duesseldorf A";
+        source address ${DUS_A_GRE_MY_IPV6};
+        neighbor ${DUS_A_GRE_BB_IPV6} as ${ffrl_as_number};
+	preference 110;
+}
+protocol bgp ffrl_dus_b from uplink {
+        description "Rheinland Backbone Duesseldorf B";
+        source address ${DUS_B_GRE_MY_IPV6};
+        neighbor ${DUS_B_GRE_BB_IPV6} as ${ffrl_as_number};
+}
+_EOF
+}
+
+
+function show_ferm_config {
+	cat << _EOF > ferm.conf.${EXT}
+domain (ip ip6) {
+    table filter {
+        chain INPUT {
+            policy ACCEPT;
+            proto gre ACCEPT;
+            mod state state INVALID DROP;
+            mod state state (ESTABLISHED RELATED) ACCEPT;
+            interface lo ACCEPT;
+            proto icmp ACCEPT;
+            proto udp dport 500 ACCEPT;
+            proto (esp) ACCEPT;
+            proto tcp dport ssh ACCEPT;
+            proto tcp dport ${MY_SSH_PORT} ACCEPT;
+        }
+        chain OUTPUT {
+            policy ACCEPT;
+            mod state state (ESTABLISHED RELATED) ACCEPT;
+        }
+        chain FORWARD {
+            policy ACCEPT;
+            mod state state INVALID DROP;
+            mod state state (ESTABLISHED RELATED) ACCEPT;
+        }
+    }
+    table mangle {
+        chain PREROUTING {
+            interface tun-ffrl-+ {
+                MARK set-mark 1;
+            }
+        }
+        chain POSTROUTING {
+            outerface tun-ffrl-+ proto tcp tcp-flags (SYN RST) SYN TCPMSS clamp-mss-to-pmtu;
+        }
+    }
+    table nat {
+        chain POSTROUTING {
+            outerface tun-ffrl-+ saddr 172.16.0.0/12 SNAT to ${my_ffrl_exit_ipv4};
+            policy ACCEPT;
+        }
+    }
+}
+_EOF
+}
+
+
+
+echo "========================================="
+echo "Konfigurationshelfer für BGP-Konzentrator"
+echo "========================================="
+
+echo -e "\n=== Allgemeine Parameter:"
+
+## @@FFRL_AS_NUMBER@@
+echo -en "\t" 
+ffrl_as_number=$(myread "AS Nummer vom FF-RL" "${FFRL_AS_NUMBER:-201701}")
+
+## @@MY_AS_NUMBER@@
+echo -en "\t" 
+my_as_number=$(myread "Eigene AS Nummer" "${MY_AS_NUMBER}")
+
+## @@MY_FFRL_EXIT_IPV4@@
+echo -en "\t" 
+my_ffrl_exit_ipv4=$(myread "Zugewiesene FFRL-IPV4-Exit-Adresse" "${MY_FFRL_EXIT_IPV4}")
+
+# @@MY_PUBLIC_IPV4@@
+# TODO: Adresse automatisch ermitteln
+echo -en "\t" 
+my_public_ipv4=$(myread "Eigene öffentliche IPV4 Adresse" "${MY_PUBLIC_IPV4}")
+
+echo -en "\t"
+MY_SSH_PORT=$(myread "Eigener SSH-Port" "${MY_SSH_PORT}")
+
+
+TUNNELENDPOINTS="BER_A BER_B DUS_A DUS_B"
+
+for bb_endpoint in ${TUNNELENDPOINTS}; do 
+	echo -e "\n=== Konfiguration für GRE-Tunnel nach ${bb_endpoint}:"
+	echo -en "\t" 
+
+	unset value
+	bar=${bb_endpoint}_GRE_BB_IPV4
+	eval $bar=\$${bar}
+	value=$(eval echo \$${bar})
+	value=$(myread "IPV4 Adresse für Tunnelendpunkt auf Backbone-Server" "${value}")
+	eval $bar=$value
+
+	echo -en "\t" 
+	unset value
+	bar=${bb_endpoint}_GRE_MY_IPV4
+	eval $bar=\$${bar}
+	value=$(eval echo \$${bar})
+	value=$(myread "IPV4 Adresse für Tunnelendpunk auf Konzentrator" "${value}") 
+	eval $bar=$value
+
+	echo -en "\t" 
+	unset value
+	bar=${bb_endpoint}_GRE_MY_IPV6
+	eval $bar=\$${bar}
+	value=$(eval echo \$${bar})
+	value=$(myread "IPV6 Adresse auf Konzentrator" "${value}") 
+	eval $bar=$value
+done
+
+echo -e "\n=== Ausgaben"
+echo -e "\tKonfigurationen geschrieben nach:"
+show_bird_config
+echo -e "\t\tbird.conf.${EXT}"
+show_bird6_config
+echo -e "\t\tbird6.conf.${EXT}"
+show_interfaces
+echo -e "\t\tinterfaces.${EXT}"
+show_ferm_config
+echo -e "\t\tferm.conf.${EXT}"
